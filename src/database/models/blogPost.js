1ff3a27ae089befa4c953258f35f19dde7e10658
Joao Keuwe <joaokeuwe@DESKTOP-19VFKNA.localdomain>
2022-05-30 17:30:57 -0300

    Requisito 10

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const BlogPost = (sequelize, DataTypes) => {
+
+ const BlogPost = sequelize.define("BlogPost",{
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'BlogPost',
+  });
+  BlogPost.associate = (models) => {
+		BlogPost.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
+	};
+
+  return BlogPost;
+};
+
+module.exports = BlogPost;
--- src/database/models/user.js
+++ src/database/models/user.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
     password: DataTypes.STRING,
     image: DataTypes.STRING
   }, {
-   timestamps: false,
+    timestamps: false
   });
+  User.associate = (models) => {
+		User.hasMany(models.BlogPost, { foreignKey: 'userId', as: 'post' });
+	};
   return User;
 };
